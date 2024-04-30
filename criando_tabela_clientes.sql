e2e1c8f63e94081e278f88e347f61e87c6096c0e
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-04-30 16:32:51 -0300

    criando as primeiras tabelas

---
+++ criando_tabela_clientes.sql
@@ -0,0 +1,51 @@
+-- CRIANDO AS TABELAS DO BANCO
+
+/*Colunas:
+CPF;
+Nome completo;
+Endereço completo;
+Data de nascimento;
+Idade;
+Sexo;
+Limite de crédito;
+Volume mínimo de compra de produto;
+Se já realizou alguma compra na empresa.*/
+
+CREATE TABLE [TABELA DE CLIENTES](
+
+CPF CHAR (11),
+NOME VARCHAR (50),
+RUA VARCHAR (50),
+COMPLEMENTO VARCHAR (50),
+BAIRRO VARCHAR (50),
+ESTADO CHAR (2),
+CEP CHAR (8),
+[DATA DE NASCIMENTO] DATE,
+IDADE SMALLINT,
+SEXO CHAR (1),
+[LIMITE DE CREDITO] MONEY,
+[VOLUME MINIMO] FLOAT,
+[PRIMEIRA COMPRA] BIT -- Valor booleano
+
+
+);
+
+/*Colunas:
+código do produto;
+nome do produto;
+embalagem;
+tamanho;
+sabor;
+preço de lista.
+*/
+
+CREATE TABLE [TABELA DE PRODUTOS](
+
+[CODIGO DO PRODUTO] VARCHAR (20) NOT NULL PRIMARY KEY,
+[NOME DO PRODUTO] VARCHAR (50),
+EMBALAGEM VARCHAR (20),
+TAMANHO VARCHAR (20),
+SABOR VARCHAR (20),
+[PRECO DE LISTA] SMALLMONEY
+
+);
